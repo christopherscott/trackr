9d6f52bae091f99f1755b21efd8588c8f337b195
Chris Hernandez <c.scott.hernandez@gmail.com>
2015-06-26 08:50:22 -0500

    updated with comments

--- trackr.js
+++ trackr.js
@@ -1,14 +1,16 @@
 import Fluxible from 'fluxible';
 import Application from './components/Application';
 import ApplicationStore from './stores/ApplicationStore';
-import {RouteStore} from 'fluxible-router';
+import { RouteStore } from 'fluxible-router';
 import routes from './routes';
 
-// create new fluxible trackr instance
-const trackr = new Fluxible({ component: Application });
+// 1. create route store
 const AppRouteStore = RouteStore.withStaticRoutes(routes);
 
-// register stores
+// 2. create new fluxible trackr instance
+const trackr = new Fluxible({ component: Application });
+
+// 3. register stores
 trackr.registerStore(AppRouteStore);
 trackr.registerStore(ApplicationStore);
 
